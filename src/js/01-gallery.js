333e99c1d3aae3d89d597d8c543070b267c55a4a
Svitlana Kravets <svetakravez72@gmail.com>
2022-12-17 18:45:39 +0200

    done task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//console.log(galleryItems);
+
+const divGallery = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    (acc =
+      acc +
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`),
+  ''
+);
+
+divGallery.innerHTML += markup;
+
+new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'botton',
+  captionDelay: 250,
+  animationSpeed: 500,
+  fadeSpeed: 200,
+});
